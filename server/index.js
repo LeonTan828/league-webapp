063c9b538fd0392f4beb59860bc49c960e68feb8
Leon Tan <yltan828@gmail.com>
2025-11-22 16:55:43 -0700

    use express on server index file

--- server/index.js
+++ server/index.js
@@ -1,10 +1,31 @@
-import http from 'http'
+import express from 'express'
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('Hello World')
+const app = express()
+
+const scores = [
+  {
+    name: 'John Doe',
+    score: 10,
+  },
+  {
+    name: 'James Smith',
+    score: 20,
+  },
+  {
+    name: 'Jane Doe',
+    score: 30,
+  },
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/scores', (request, response) => {
+  response.json(scores)
 })
 
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
